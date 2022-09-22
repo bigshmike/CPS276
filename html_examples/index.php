c66ff2e84735f8793893466bf229c42bc5e307ca
bigshmikee <mbrown99@wccnet.edu>
2022-09-22 15:51:23 -0400

    changes

---
+++ html_examples/index.php
@@ -0,0 +1,34 @@
+<!--
+<?php
+
+$name = "Michael"
+
+?>
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Elements</title>
+</head>
+<body>
+   <h1>Hello World!</h1> 
+   <!--<p><?php echo $name ?></p> -->
+   <p id="someID" class="someClass">This is a paragraph with an ID and a class defined.</p>
+   <header>
+        <div>
+            <h1>H1</h1>
+                <pre>
+                    This is a test of a pre-defined text element .    . .
+
+
+
+                    You can have m u l t i p l e spaces between lines and characters.
+                </pre>  
+        </div>
+    </header>
+</body>
+</html>
