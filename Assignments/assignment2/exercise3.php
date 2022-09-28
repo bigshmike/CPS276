6646ec2eedc26686dc8ab2921e63fc7277008407
bigshmikee <mbrown99@wccnet.edu>
2022-09-28 14:26:46 -0400

    created

---
+++ Assignments/assignment2/exercise3.php
@@ -0,0 +1,29 @@
+<?php
+function createTable() {
+    $tableBorder = 1;
+    $numOfRows = 15;
+    $numOfColumns = 5;
+    echo "<table border=$tableBorder>";
+    for($i = 1; $i <= $numOfRows; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= $numOfColumns; $j++) {
+            echo "<td> Row " . $i . " Column " . $j . "</td>"; 
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php createTable(); ?></p>
+</body>
+</html>
