5fe77c56269578bae80b7fc5c171292175587941
bigshmikee <mbrown99@wccnet.edu>
2022-09-28 14:27:33 -0400

    updates

--- Assignments/assignment2/exercise1.php
+++ Assignments/assignment2/exercise1.php
@@ -20,7 +20,7 @@ function orderedListIncrementer() {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercise 1</title>
 </head>
 
 <body>
