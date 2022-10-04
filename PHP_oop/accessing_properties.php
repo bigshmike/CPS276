b2d91620824b898275fb9b2a7165fdeaa1020423
bigshmikee <mbrown99@wccnet.edu>
2022-10-04 19:36:00 -0400

    examples

---
+++ PHP_oop/accessing_properties.php
@@ -0,0 +1,14 @@
+<?php
+class MyClass {
+    private $greeting = "Hello, World!";
+
+    public function hello() {
+        return $this->greeting;
+    }
+    
+}
+
+
+$obj = new MyClass;
+echo $obj->hello(); // DISPLAYS "HELLO, WORLD!"
+?>
---
+++ PHP_oop/static_methods.php
@@ -0,0 +1,10 @@
+<?php
+
+//STATIC METHODS MUST BE PUBLIC
+class Car {
+    public static function calcMpg( $miles, $gallons ) {
+        return ( $miles / $gallons );
+    }
+}
+    echo Car::calcMpg( 168, 6 );
+?>
