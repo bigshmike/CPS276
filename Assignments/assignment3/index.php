aca50f5d624e6287f36a7f5d3991febadb93d6e0
bigshmikee <mbrown99@wccnet.edu>
2022-10-05 11:32:46 -0400

    removed comments

--- Assignments/assignment3/index.php
+++ Assignments/assignment3/index.php
@@ -1,11 +1,11 @@
 <?php
 require_once "Calculator.php";
 $Calculator = new Calculator();
-echo $Calculator->calc("/", 10, 0) . "<br>"; //will output Cannot divide by zero
-echo $Calculator->calc("*", 10, 2) . "<br>"; //will output The product of the numbers is 20
-echo $Calculator->calc("/", 10, 2) . "<br>"; //will output The division of the numbers is 5
-echo $Calculator->calc("-", 10, 2) . "<br>"; //will output The difference of the numbers is 8 
-echo $Calculator->calc("+", 10, 2) . "<br>"; //will output The sum of the numbers is 12
-echo $Calculator->calc("*", 10) . "<br>"; //will output You must enter a string and two numbers 
-echo $Calculator->calc(10); //will output You must enter a string and two numbers
+echo $Calculator->calc("/", 10, 0) . "<br>"; 
+echo $Calculator->calc("*", 10, 2) . "<br>"; 
+echo $Calculator->calc("/", 10, 2) . "<br>"; 
+echo $Calculator->calc("-", 10, 2) . "<br>"; 
+echo $Calculator->calc("+", 10, 2) . "<br>"; 
+echo $Calculator->calc("*", 10) . "<br>"; 
+echo $Calculator->calc(10); 
 ?>
