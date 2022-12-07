3858f41ac3c6fd415030e89d413c29017264ced0
bigshmike <mbrown99@wccnet.edu>
2022-12-07 08:59:24 -0500

    minor tweak

--- Assignments/assignment10/pages/welcome.php
+++ Assignments/assignment10/pages/welcome.php
@@ -1,8 +1,6 @@
 <?php
 
 function init() {
-    //session_start();
     $name = $_SESSION['name'];
-    $status = $_SESSION['status'];
-    return ["<h1>Welcome</h1>", "<p>Welcome $name, you are $status </p>"];
+    return ["<h1>Welcome</h1>", "<p>Welcome $name</p>"];
 }
